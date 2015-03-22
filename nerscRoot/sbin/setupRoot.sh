af55cd5b459a9e8c34dc97bbf749bc73de15b7db
Doug Jacobsen <dmjacobsen@lbl.gov>
2015-03-22 07:06:10 -0700

    improve error checking, variable localization, started adding user-specified bind-mounts (untested), moved to configuration file instead of explicit config, improve argument parsing

--- nerscRoot/sbin/setupRoot.sh
+++ nerscRoot/sbin/setupRoot.sh
@@ -14,39 +14,110 @@ export PATH="/usr/bin:/bin"
 export LD_LIBRARY_PATH=""
 export LD_PRELOAD=""
 
+## read configuration
+CONFIG_FILE=/global/syscom/sc/nsg/etc/nerscRoot.conf
+if [ -e $CONFIG_FILE ]; then
+    . $CONFIG_FILE
+fi
+
+## parse command line arguments
 NERSC_ROOT_TYPE=$1
 NERSC_ROOT_VALUE=$2
+shift 2
+VOLUMES=""
+while getopts ":v:" opt; do
+    case $opt in
+        v)
+            if [ -z $VOLUMES ]; then
+                VOLUMES=$OPTARG
+            else
+                VOLUMES="$VOLUMES|$OPTARG"
+            fi
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument" >&2
+            exit 1
+            ;;
+    esac
+done
 
-nerscMount=/var/nerscMount
-dockerVFSPath=/scratch/docker/vfs/dir
-chosPath=/scratch/chos
-nerscRootPath=/global/syscom/sc/nsg/opt/nerscRoot
-mapPath=$nerscRootPath/fsMap.conf
-etcDir=$nerscRootPath/etc
-kmodDir=$nerscRootPath/kmod/$( uname -r )
-validTargets="chosVFS dockerVFS chosExt4Image dockerExt4Image"
+#### TODO CHECK CONFIGURATION SETTINGS ####
+#nerscMount=/var/nerscMount
+#chosPath=/scratch/chos
+#dockerPath=/scratch/scratchdirs/craydock/docker
+#nerscRootPath=/global/syscom/sc/nsg/opt/nerscRoot
+#mapPath=$nerscRootPath/fsMap.conf
+#etcDir=$nerscRootPath/etc
+#kmodDir=$nerscRootPath/kmod/$( uname -r )
+#kmodCache=/tmp/nerscRootLoadedModules.txt
+#globalFs="u1 u2 project syscom common scratch2" 
+#lustreFs="scratch"
 
 targetType=""
 target=""
-
-globalFs="u1 u2 project syscom common" 
-lustreFs="scratch"
-
 
 die () {
     echo $1
     exit 1
 }
 
+stripLeadingSlash() {
+    local string
+    string=$1
+    while [ "x${string:0:1}" == "x/" ]; do
+        string=${string:1}
+    done
+    echo $string
+}
+
+containsItem () {
+    local tgt
+    tgt=$1
+    shift
+    [[ $# -eq 0 ]] && return 1
+    while true; do
+        [[ $1 == $tgt ]] && return 0
+        [[ $# -eq 0 ]] && break
+        shift
+    done
+    return 1
+}
+
+containsItemStartsWith() {
+    local tgt
+    tgt=$1
+    shift
+    [[ $# -eq 0 ]] && return 1
+    while true; do
+        case $tgt in
+            "$1"*) return 0 ;;
+        esac
+        [[ $# -eq 0 ]] && break
+        shift
+    done
+    return 1
+}
+
 setupNerscVFSRoot () {
     local fs
     local dir
     local item
+    local imageDir
+    local volMap
+    local src
+    local dest
+    local option
+    local ok
+    local REGIFS
     imageDir=$1
 
     ## create tmpfs/rootfs for our /
     ## this is to ensure there are writeable areas for manipulating the image
-    mount -t rootfs none $nerscMount
+    mount -o nosuid,nodev -t rootfs none $nerscMount
     cd $nerscMount
 
     ## setup NERSC GPFS filesystems
@@ -54,7 +125,7 @@ setupNerscVFSRoot () {
     for fs in $globalFs; do 
         mkdir global/$fs
         mount -o bind /global/$fs global/$fs
-        mount -o remount,nodev,nosuid $nerscMount/global/$fs
+        mount -o remount,nosuid,nodev $nerscMount/global/$fs
     done
     cd global
     ln -s u1 homes
@@ -81,16 +152,25 @@ setupNerscVFSRoot () {
     mkdir -p sys
     mkdir -p dev
     mkdir -p tmp
+    mount -o bind /tmp tmp
     
     # mount the image into the new mount
     for dir in `ls $imageDir`; do
+        # don't do anything to "reserved" paths
         if [ -e $dir ]; then
             continue
         fi
+        # properly copy symlinks
         if [ -L $imageDir/$dir ]; then
-            cp $imageDir/$dir $dir
+            cp -P $imageDir/$dir $dir
             continue
         fi
+        # copy files
+        if [ -f $imageDir/$dir ]; then
+            cp -p $imageDir/$dir $dir
+            continue
+        fi
+        # bind mount directories
         if [ -d $imageDir/$dir ]; then
             mkdir $dir
             mount --bind $imageDir/$dir $nerscMount/$dir
@@ -129,38 +209,80 @@ setupNerscVFSRoot () {
     mount -o bind /dev $nerscMount/dev
     mount -o bind /sys $nerscMount/sys
 
+    ## perform any user-requested bind-mounts
+    ##    any leading slashes are stripped to force the bind mount to *only*
+    ##    occur within the chroot area
+    REGIFS=$IFS
+    IFS="|"
+    for volMap in $VOLUMES; do
+        IFS=":"
+        set x $volMap
+        src=$(stripLeadingSlash $2)
+        dest=$(stripLeadingSlash $3)
+        option=NONE
+        shift 3
+        if [ ! -z $1 ]; then
+            option=$1
+            shift
+        fi
+        IFS=$REGIFS
+        if [ -e $src -a -e $dest ]; then
+            mount -o bind $src $dest
+            if [ "x$option" == "xro" ]; then
+                ok=containsItemStartsWith $src $lustreFs
+                if [ $ok -eq 0 ]; then
+                    echo "Fail: cannot mark lustre filesystems read-only"
+                else
+                    mount -o remount,ro $dest
+                fi
+            fi
+        fi
+        IFS="|"
+    done
+    IFS=$REGIFS
+}
+
+## loadKernelModule - ensure specified kernel module is loaded
+##   checks to see if kernel module is loaded.  if it isn't the module is loaded
+##   and logged to the kmodCache for later removal
+##
+##   returns 0 if module is loaded
+##   returns 1 if module failed to load
+loadKernelModule() {
+    local kmodName
+    local kmodPath
+    local loadModule
+    kmodName=$1
+    kmodPath=$2
+    loadModule=0
+    /sbin/lsmod | egrep "^$kmodName$" || loadModule=1
+    if [ $loadModule -eq 1 -a -e $kmodPath ]; then
+        /sbin/insmod $kmodPath || return 1
+        echo $kmodName >> $kmodCache
+    fi
+    return 0
 }
 
 setupLoopbackMount() {
     local imageFile
     local kmodPath
+    local fstype
     imageFile=$1
     kmodPath=$2
+    fstype=$3
 
-    /sbin/insmod $kmodPath/drivers/block/loop.ko
-    /sbin/insmod $kmodPath/fs/mbcache.ko
-    /sbin/insmod $kmodPath/fs/jbd2/jbd2.ko
-    /sbin/insmod $kmodPath/fs/ext4/ext4.ko
+    [[ -n $loopMount ]] || die "Unknown location for loopMount"
 
-    loopMount=/var/loopNerscMount
-    mkdir -p $loopMount
-    mount -t ext4 -o loop,ro $imageFile $loopMount || die "Failed to mount image file $imageFile"
+    loadKernelModule "loop"    $kmodPath/drivers/block/loop.ko
+    loadKernelModule "mbcache" $kmodPath/fs/mbcache.ko
+    loadKernelModule "jbd2"    $kmodPath/fs/jbd2/jbd2.ko
+    loadKernelModule "ext4"    $kmodPath/fs/ext4/ext4.ko
+    loadKernelModule "cramfs"  $kmodPath/fs/cramfs/cramfs.ko
+
+    mkdir -p $loopMount || die "Failed to create mount point $loopMount"
+    mount -t $fstype -o loop,ro,nodev,nosuid $imageFile $loopMount || die "Failed to mount image file $imageFile"
     setupNerscVFSRoot $loopMount
 }
-
-containsItem () {
-    local tgt
-    tgt=$1
-    shift
-    [[ $# -eq 0 ]] && return 1
-    while true; do
-        [[ $1 == $tgt ]] && return 0
-        [[ $# -eq 0 ]] && break
-        shift
-    done
-    return 1
-}
-
 
 
 if [ "x$CHOS" != "x" ]; then
@@ -189,11 +311,16 @@ containsItem $targetType "chos" "docker" || die "Invalid image target type: $tar
 target=${target//[^a-zA-Z0-9_:\.]/}
 
 linePrefix="$targetType;$target;"
-line=$( egrep "^$linePrefix" $mapPath ) || die "Cannot find $targetType image \"$target\". Failed."
-image=$( echo $line | awk -F ';' '{print $3}' ) || die "Cannot identify path for $targetType image \"$target\". Failed."
-imageType=$( echo $line | awk -F ';' '{print $4}' ) || die "Cannot identify imageType for $targetType image \"$target\". Failed."
+if [ "x$targetType" == "xdocker" ]; then
+    imageType="ext4Image"
+    image="$target.ext4"
+else
+    line=$( egrep "^$linePrefix" $mapPath ) || die "Cannot find $targetType image \"$target\". Failed."
+    image=$( echo $line | awk -F ';' '{print $3}' ) || die "Cannot identify path for $targetType image \"$target\". Failed."
+    imageType=$( echo $line | awk -F ';' '{print $4}' ) || die "Cannot identify imageType for $targetType image \"$target\". Failed."
+fi
 
-containsItem $imageType "vfs" "ext4Image" || die "Invalid imageType for $targetType image \"$target\". Failed."
+containsItem $imageType "vfs" "ext4Image" "cramfsImage" || die "Invalid imageType for $targetType image \"$target\". Failed."
 
 ## get base path for this target type
 pathVar="${targetType}Path"
@@ -203,14 +330,16 @@ eval basePath=\$$pathVar
 
 ## get final path to image
 fullPath="${basePath}/${image}"
-[[ -e $fullPath ]] || die "Path to $targetType image \"$target\" does not exist"
+[[ -e $fullPath ]] || die "Path to $targetType image \"$target\" does not exist: $fullPath"
 
 ## start doing bind mounts
 mkdir -p $nerscMount
 if [ "x$imageType" == "xvfs" ]; then
     setupNerscVFSRoot $fullPath
 elif [ "x$imageType" == "xext4Image" ]; then
-    setupLoopbackMount $fullPath $kmodDir
+    setupLoopbackMount $fullPath $kmodDir "ext4"
+elif [ "x$imageType" == "xcramfsImage" ]; then
+    setupLoopbackMount $fullPath $kmodDir "cramfs"
 else
     exit 1
 fi
--- nerscRoot/sbin/unsetupRoot.sh
+++ nerscRoot/sbin/unsetupRoot.sh
@@ -4,20 +4,34 @@ export PATH="/usr/bin:/bin"
 export LD_LIBRARY_PATH=""
 export LD_PRELOAD=""
 
-nerscMount=/var/nerscMount
-loopMount=/var/loopNerscMount
+CONFIG_FILE=/global/syscom/sc/nsg/etc/nerscRoot.conf
+if [ -e $CONFIG_FILE ]; then
+    . $CONFIG_FILE
+fi
+
+if [ -z $nerscMount ]; then
+    nerscMount=/var/nerscMount
+fi
+if [ -z $loopMount ]; then
+    loopMount=/var/loopNerscMount
+fi
 ret=0
 
 for i in `cat /proc/mounts | grep $nerscMount | sort -k2 -r | awk '{print $2}'`; do
     umount $i || ret=1
 done
 
-grep $loopMount /proc/mounts >/dev/null 2>&1 || exit $ret 
+grep $loopMount /proc/mounts >/dev/null 2>&1 || ret=1
 
 ## must be a loop mount to clean up
 umount $loopMount || ret=1
-/sbin/rmmod ext4
-/sbin/rmmod loop
-/sbin/rmmod jbd2
-/sbin/rmmod mbcache
+
+## remove any kernel modules loaded (in reverse order)
+if [ -n $kmodCache -a -e "$kmodCache" ]; then
+    for kmod in `cat $kmodCache | awk '{ L[n++] = $0 } END{ while(n--) { print L[n] } }'`; do
+        echo /sbin/rmmod $kmod
+        /sbin/rmmod $kmod
+    done
+    rm $kmodCache
+fi
 exit $ret
