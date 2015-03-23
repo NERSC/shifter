5abb56c87e4257eb3138d6bd8a311dd72421fb37
Doug Jacobsen <dmjacobsen@lbl.gov>
2015-03-23 06:49:33 -0700

    add some error handling to getDockerImage, begin building support for progress output

--- nerscRoot/getDockerImage.pl
+++ nerscRoot/getDockerImage.pl
@@ -4,6 +4,7 @@ use IO::Socket::INET;
 
 my $dockergw_host = "128.55.50.83";
 my $dockergw_port = "7777";
+my $ret = 0;
 
 if (scalar(@ARGV) != 1) {
     usage(1);
@@ -12,16 +13,37 @@ my $image = $ARGV[0];
 if ($image !~ /:/) {
     usage(1);
 }
-my $nerscHost = `cat /etc/clustername`;
+my $system = undef;
+if (-e "/etc/clustername") {
+    $system = `cat /etc/clustername`;
+}
+if (!defined($system)) {
+    print STDERR "Unknown system.\n";
+    usage(1);
+}
+
 my $socket = new IO::Socket::INET (
     PeerHost => $dockergw_host,
     PeerPort => $dockergw_port,
     Proto => 'tcp',
-) or exit 1;
+) or die("Failed to connect to dockergw");
 $socket->send("$image $nerscHost\n");
-my $result = <$socket>;
+my $result = "";
+while (<$socket>) {
+    my $data = $_;
+    $data =~ s/^s\+//g;
+    $data =~ s/\s+$//g;
+    if ($data =~ /^ID:\s+(\S+)$/) {
+        $result = $1;
+    }
+    if ($data =~ /^ERR:\s+(.*)$/) {
+        $result = $1;
+        $ret = 1;
+    }
+}
 $socket->close();
-print $result;
+print "$result\n";
+exit $ret;
 
 
 sub usage {
