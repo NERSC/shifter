953eebef8f9a6b48cb1254fdf68bfcfa7e657a12
Doug Jacobsen <dmjacobsen@lbl.gov>
2015-03-23 18:51:10 -0700

    add config file as common include for Makefiles

---
+++ dockerRoot/config.mk
@@ -0,0 +1,7 @@
+PREFIX=/global/syscom/sc/nsg/opt/dockerRoot
+CONFIG_DIR=/global/syscom/sc/nsg/etc
+CONFIG_FILE=$(CONFIG_DIR)/dockerRoot.conf
+INCLUDE_FILE=$(CONFIG_DIR)/dockerRoot.include
+CRAY_NETWORK=gem
+CRAY_NODETYPE=c
+SYSTEM=grace
