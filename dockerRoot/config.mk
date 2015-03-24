b9d9baa813720f70447ea1f78b7381c489153011
Doug Jacobsen <dmjacobsen@lbl.gov>
2015-03-23 22:34:42 -0700

    fix utility paths in Makefile

--- dockerRoot/config.mk
+++ dockerRoot/config.mk
@@ -2,7 +2,12 @@ PREFIX=/global/syscom/sc/nsg/opt/dockerRoot
 CONFIG_DIR=/global/syscom/sc/nsg/etc
 CONFIG_FILE=$(CONFIG_DIR)/dockerRoot.conf
 INCLUDE_FILE=$(CONFIG_DIR)/dockerRoot.include
-CRAY_NETWORK=gem
+CRAY_NETTYPE=gem
 CRAY_NODETYPE=c
 SYSTEM=grace
 WLM_INT=alps
+
+CC      = /usr/bin/gcc
+RM      = /bin/rm
+CMP     = /usr/bin/cmp
+INSTALL = /usr/bin/install
