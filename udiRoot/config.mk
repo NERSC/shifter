8282cf0f63579ef009e3694d771ac2f7a37ecbfc
Doug Jacobsen <dmjacobsen@lbl.gov>
2015-03-26 06:15:07 -0700

    rename config files

--- udiRoot/config.mk
+++ udiRoot/config.mk
@@ -1,7 +1,7 @@
 PREFIX=/global/syscom/sc/nsg/opt/udiRoot
 CONFIG_DIR=/global/syscom/sc/nsg/etc
-CONFIG_FILE=$(CONFIG_DIR)/dockerRoot.conf
-INCLUDE_FILE=$(CONFIG_DIR)/dockerRoot.include
+CONFIG_FILE=$(CONFIG_DIR)/udiRoot.conf
+INCLUDE_FILE=$(CONFIG_DIR)/udiRoot.include
 CRAY_NETTYPE=gem
 CRAY_NODETYPE=c
 SYSTEM=grace
