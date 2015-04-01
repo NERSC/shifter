b07a576e554cd5cc31793d70ca495e9fc8cc2f41
Doug Jacobsen <dmjacobsen@lbl.gov>
2015-04-01 10:55:36 -0700

    sshd was crapping out with really long lines in the groups file, so now are filtered to be less than 1024; added static content like nsswitch.conf

--- udiRoot/setup_udiRoot_etc_files.sh
+++ udiRoot/setup_udiRoot_etc_files.sh
@@ -2,6 +2,7 @@
 
 mkdir -p $PREFIX/etc_files
 getent passwd > $PREFIX/etc_files/docker_passwd
-getent group > $PREFIX/etc_files/docker_group
+getent group | awk '{ if (length($0) < 1024) { print $0; } }' > $PREFIX/etc_files/docker_group
 cp /etc/resolv.conf $PREFIX/etc_files/resolv.conf
 cp /etc/clustername $PREFIX/etc_files/clustername
+cp -rp etc_static/* $PREFIX/etc_files
