b3b29cdfca8b733456d1244764aff86d1b9cc489
Doug Jacobsen <dmjacobsen@lbl.gov>
2015-04-01 12:16:37 -0700

    fix sshd termination (regression)

--- udiRoot/setup_udiRoot_etc_files.sh
+++ udiRoot/setup_udiRoot_etc_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/etc_files
-getent passwd > $PREFIX/etc_files/docker_passwd
-getent group | awk '{ if (length($0) < 1024) { print $0; } }' > $PREFIX/etc_files/docker_group
+getent passwd > $PREFIX/etc_files/udi_passwd
+getent group | awk '{ if (length($0) < 1024) { print $0; } }' > $PREFIX/etc_files/udi_group
 cp /etc/resolv.conf $PREFIX/etc_files/resolv.conf
 cp /etc/clustername $PREFIX/etc_files/clustername
 cp -rp etc_static/* $PREFIX/etc_files
