2e96ef75f6acb3ecb43a984914d9c9ee13e45ebf
Doug Jacobsen <dmjacobsen@lbl.gov>
2015-04-06 07:31:49 -0700

    limit max length of group lines to 512 characters

--- udiRoot/setup_udiRoot_etc_files.sh
+++ udiRoot/setup_udiRoot_etc_files.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $PREFIX/etc_files
 getent passwd > $PREFIX/etc_files/udi_passwd
-getent group | awk '{ if (length($0) < 1024) { print $0; } }' > $PREFIX/etc_files/udi_group
+getent group | awk '{ if (length($0) < 512) { print $0; } }' > $PREFIX/etc_files/udi_group
 cp /etc/resolv.conf $PREFIX/etc_files/resolv.conf
 cp /etc/clustername $PREFIX/etc_files/clustername
 cp -rp etc_static/* $PREFIX/etc_files
