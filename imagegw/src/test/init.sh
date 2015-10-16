48951c76817a708c02e515b9df33b1d04fed5ff6
Shane Canon <scanon@lbl.gov>
2015-10-16 14:10:28 -0700

    - Moved tests and tweaked init.sh helper

--- imagegw/src/test/init.sh
+++ imagegw/src/test/init.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+export PATH=$PATH:$(pwd)/test/
+
 function refreshmunge {
   export MUNGE=$(grep auth ~/.dockercfg |sed 's/",//'|sed 's/.*"//'|systema munge )
 }
 
-function refresha {
+function update {
   IP=$(docker-machine ip $(docker-machine active))
   PORT=$(docker-compose ps systema|grep systema|sed 's/.*://'|sed 's/-.*//')
   alias systema="ssh -i test/ssh.key $IP -p $PORT -l root"
-  systema -y date
+  echo y|ssh -i test/ssh.key $IP -p $PORT -l root date
+  export IMAGEGW="$IP:5555"
 }
