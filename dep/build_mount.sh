d3195bb72bd3a20f5cdad50fcc50026ab66801b1
Doug Jacobsen <dmjacobsen@nersc.gov>
2016-04-30 09:16:28 -0700

    create option to disable staticsshd if that is desirable; break up dependency builds; move test program builds into "make check"

---
+++ dep/build_mount.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+unset CFLAGS
+unset CPPFLAGS
+
+origdir=$( pwd )
+mkdir -p build_mount
+cd build_mount
+builddir=$( pwd )
+
+if [[ ! -e "util-linux-2.26.2.tar.gz" ]]; then
+    curl -k -o "util-linux-2.26.2.tar.gz" "https://www.kernel.org/pub/linux/utils/util-linux/v2.26/util-linux-2.26.2.tar.gz"
+fi
+
+cd "${builddir}"
+mkdir -p util-linux
+tar xf "util-linux-2.26.2.tar.gz" -C util-linux --strip-components=1
+cd util-linux
+CC=gcc ./configure --enable-static --disable-shared
+CC=gcc make mount
+cp -p mount "${origdir}/"
