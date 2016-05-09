cef9559aecca301c53fb77e0836fc6152851e995
Douglas Jacobsen <dmjacobsen@lbl.gov>
2016-05-09 03:44:06 -0700

    fix typo

--- dep/build_ssh.sh
+++ dep/build_ssh.sh
@@ -93,7 +93,7 @@ cd openssh
 ## the image is not infected with all kinds of silly paths (sshd sets PATH to
 ## very nearly the path it was built with)
 export PATH="/usr/bin:/bin"
-LDFLAGS="${SPRT_PREFIX}/lib ${SPRT_PREFIX}/lib64" CC="${SPRT_PREFIX}/bin/musl-gcc" ./configure --without-pam "--with-ssl-dir=${SPRT_PREFIX}" --without-ssh1 --enable-static --disable-shared "--with-zlib=${SPRT_PREFIX}" "--prefix=${INST_PREFIX}"
+LDFLAGS="-L${SPRT_PREFIX}/lib -L${SPRT_PREFIX}/lib64" CC="${SPRT_PREFIX}/bin/musl-gcc" ./configure --without-pam "--with-ssl-dir=${SPRT_PREFIX}" --without-ssh1 --enable-static --disable-shared "--with-zlib=${SPRT_PREFIX}" "--prefix=${INST_PREFIX}"
 make
 make install "DESTDIR=${PREFIX}"
 cd "${builddir}"
