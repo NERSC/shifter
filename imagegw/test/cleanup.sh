6bfb5e9cf94389c193b391b36c32ae6cc3693c68
Shane Canon <scanon@lbl.gov>
2016-05-20 08:40:17 -0700

    Fixes in mock scripts and cleanup

--- imagegw/test/cleanup.sh
+++ imagegw/test/cleanup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-ps auxww|grep celery|grep systema|grep -v grep|awk '{print $2}'
-ps auxww|grep celery|grep systema|grep -v grep|awk '{print $2}'|xargs kill
+pids=$(ps auxww|grep celery|grep systema|grep -v grep|awk '{print $2}')
+if [ ! -z $pids ] ; then
+  echo "Killing $pids"
+  ps auxww|grep celery|grep systema|grep -v grep|awk '{print $2}'|xargs kill
+fi
+
 exit 0
