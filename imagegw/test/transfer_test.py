eb42edb26a1517449c8f793c6b8886db53a45dee
Douglas Jacobsen <dmjacobsen@lbl.gov>
2016-08-21 15:54:19 -0700

    pylint: clean up transfer_test

--- imagegw/test/transfer_test.py
+++ imagegw/test/transfer_test.py
@@ -1,32 +1,29 @@
+# Shifter, Copyright (c) 2016, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#  3. Neither the name of the University of California, Lawrence Berkeley
+#     National Laboratory, U.S. Dept. of Energy nor the names of its
+#     contributors may be used to endorse or promote products derived from this
+#     software without specific prior written permission.`
+#
+# See LICENSE for full text.
+
 import os
-import sys
 import unittest
 import tempfile
-
-"""
-Shifter, Copyright (c) 2016, The Regents of the University of California,
-through Lawrence Berkeley National Laboratory (subject to receipt of any
-required approvals from the U.S. Dept. of Energy).  All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
- 1. Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
- 2. Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
- 3. Neither the name of the University of California, Lawrence Berkeley
-    National Laboratory, U.S. Dept. of Energy nor the names of its
-    contributors may be used to endorse or promote products derived from this
-    software without specific prior written permission.`
-
-See LICENSE for full text.
-"""
-
 from shifter_imagegw import transfer
 
 class TransferTestCase(unittest.TestCase):
     system = {}
+    inodes = 0
 
     def setUp(self):
         self.system['host'] = ['localhost']
@@ -39,7 +36,6 @@ class TransferTestCase(unittest.TestCase):
             'imageDir': None
         }
         self.system['accesstype'] = 'local'
-        pass
 
     def tearDown(self):
         """
@@ -52,7 +48,7 @@ class TransferTestCase(unittest.TestCase):
         assert len(cmd) == 2
         assert cmd[0] == 'echo'
         assert cmd[1] == 'test'
-     
+
         cmd = transfer._sh_cmd(self.system, 'anotherCommand')
         assert len(cmd) == 1
         assert cmd[0] == 'anotherCommand'
@@ -92,128 +88,129 @@ class TransferTestCase(unittest.TestCase):
         assert '|'.join(cmd) == 'scp|-i|somefile|-t|a|nobody@localhost:b'
         del self.system['ssh']['scpCmdOptions']
 
-    def inodeCounter(self, ignore, dirname, fnames):
+    def inode_counter(self, ignore, dirname, fnames):
         self.inodes += len(fnames)
 
     def test_copyfile_local(self):
-        tmpPath = tempfile.mkdtemp() 
-        self.system['local']['imageDir'] = tmpPath
-        self.system['ssh']['imageDir'] = tmpPath
-       
+        tmp_path = tempfile.mkdtemp()
+        self.system['local']['imageDir'] = tmp_path
+        self.system['ssh']['imageDir'] = tmp_path
+
         self.system['accesstype'] = 'local'
         transfer.copy_file(__file__, self.system)
-        dir,fname = os.path.split(__file__)
-        filePath = os.path.join(tmpPath, fname)
-        assert os.path.exists(filePath)
+        fname = os.path.split(__file__)[1]
+        file_path = os.path.join(tmp_path, fname)
+        assert os.path.exists(file_path)
 
         self.inodes = 0
-        os.path.walk(tmpPath, self.inodeCounter, None)
+        os.path.walk(tmp_path, self.inode_counter, None)
         assert self.inodes == 1
-        os.unlink(filePath)
+        os.unlink(file_path)
 
         self.inodes = 0
-        os.path.walk(tmpPath, self.inodeCounter, None)
+        os.path.walk(tmp_path, self.inode_counter, None)
         assert self.inodes == 0
 
-        os.rmdir(tmpPath)
+        os.rmdir(tmp_path)
 
     def test_copyfile_remote(self):
         """uses mock ssh/scp wrapper to pretend to do the remote
            transfer, ensure it is in PATH prior to running test
         """
-        tmpPath = tempfile.mkdtemp() 
-        self.system['local']['imageDir'] = tmpPath
-        self.system['ssh']['imageDir'] = tmpPath
-       
+        tmp_path = tempfile.mkdtemp()
+        self.system['local']['imageDir'] = tmp_path
+        self.system['ssh']['imageDir'] = tmp_path
+
         self.system['accesstype'] = 'remote'
         transfer.copy_file(__file__, self.system)
-        dir,fname = os.path.split(__file__)
-        filePath = os.path.join(tmpPath, fname)
-        assert os.path.exists(filePath)
+        fname = os.path.split(__file__)[1]
+        file_path = os.path.join(tmp_path, fname)
+        assert os.path.exists(file_path)
 
         self.inodes = 0
-        os.path.walk(tmpPath, self.inodeCounter, None)
+        os.path.walk(tmp_path, self.inode_counter, None)
         assert self.inodes == 1
-        os.unlink(filePath)
+        os.unlink(file_path)
 
         self.inodes = 0
-        os.path.walk(tmpPath, self.inodeCounter, None)
+        os.path.walk(tmp_path, self.inode_counter, None)
         assert self.inodes == 0
 
-        os.rmdir(tmpPath)
+        os.rmdir(tmp_path)
 
     def test_copyfile_invalid(self):
-        tmpPath = tempfile.mkdtemp() 
-        self.system['local']['imageDir'] = tmpPath
-        self.system['ssh']['imageDir'] = tmpPath
-       
+        tmp_path = tempfile.mkdtemp()
+        self.system['local']['imageDir'] = tmp_path
+        self.system['ssh']['imageDir'] = tmp_path
+
         self.system['accesstype'] = 'invalid'
-     
+
         with self.assertRaises(NotImplementedError):
             transfer.copy_file(__file__, self.system)
 
-        os.rmdir(tmpPath)
+        os.rmdir(tmp_path)
 
     def test_copyfile_failtowrite(self):
-        tmpPath = tempfile.mkdtemp() 
-        self.system['local']['imageDir'] = tmpPath
-        self.system['ssh']['imageDir'] = tmpPath
-       
+        tmp_path = tempfile.mkdtemp()
+        self.system['local']['imageDir'] = tmp_path
+        self.system['ssh']['imageDir'] = tmp_path
+
         self.system['accesstype'] = 'local'
 
         # make directory unwritable
-        os.chmod(tmpPath, 0555)
+        os.chmod(tmp_path, 0555)
         with self.assertRaises(OSError):
             transfer.copy_file(__file__, self.system)
 
         self.inodes = 0
-        os.path.walk(tmpPath, self.inodeCounter, None)
+        os.path.walk(tmp_path, self.inode_counter, None)
         assert self.inodes == 0
-        os.rmdir(tmpPath)
+        os.rmdir(tmp_path)
 
     def test_transfer_local(self):
-        tmpPath = tempfile.mkdtemp() 
-        self.system['local']['imageDir'] = tmpPath
-        self.system['ssh']['imageDir'] = tmpPath
-       
+        tmp_path = tempfile.mkdtemp()
+        self.system['local']['imageDir'] = tmp_path
+        self.system['ssh']['imageDir'] = tmp_path
+
         self.system['accesstype'] = 'local'
 
         transfer.transfer(self.system, __file__)
 
         ## make sure transferred file exists
-        dir,fname = os.path.split(__file__)
-        filePath = os.path.join(tmpPath, fname)
-        assert os.path.exists(filePath)
+        fname = os.path.split(__file__)[1]
+        file_path = os.path.join(tmp_path, fname)
+        assert os.path.exists(file_path)
 
         self.inodes = 0
-        os.path.walk(tmpPath, self.inodeCounter, None)
+        os.path.walk(tmp_path, self.inode_counter, None)
         assert self.inodes == 1
-        os.unlink(filePath)
+        os.unlink(file_path)
 
-        dname,fname = os.path.split(__file__)
+        dname, fname = os.path.split(__file__)
         meta = os.path.join(dname, '__init__.py')
-        
+
         transfer.transfer(self.system, __file__, meta)
         self.inodes = 0
-        os.path.walk(tmpPath, self.inodeCounter, None)
+        os.path.walk(tmp_path, self.inode_counter, None)
         assert self.inodes == 2
 
-        filePath = os.path.join(tmpPath, fname)
-        metaPath = os.path.join(tmpPath, '__init__.py')
+        file_path = os.path.join(tmp_path, fname)
+        meta_path = os.path.join(tmp_path, '__init__.py')
 
-        os.unlink(filePath)
-        os.unlink(metaPath)
-        os.rmdir(tmpPath)
+        os.unlink(file_path)
+        os.unlink(meta_path)
+        os.rmdir(tmp_path)
 
     def test_remove_local(self):
-        (fd,tmpPath) = tempfile.mkstemp() 
-        dname,fname = os.path.split(tmpPath)
+        (fdesc, tmp_path) = tempfile.mkstemp()
+        os.close(fdesc)
+        dname, fname = os.path.split(tmp_path)
         self.system['local']['imageDir'] = dname
         self.system['ssh']['imageDir'] = dname
         self.system['accesstype'] = 'local'
 
-        st=transfer.remove_file(fname, self.system)
-        self.assertEquals(os.path.exists(tmpPath),False)
+        transfer.remove_file(fname, self.system)
+        self.assertEquals(os.path.exists(tmp_path), False)
 
     # TODO: Add a test_remove_remote
 
