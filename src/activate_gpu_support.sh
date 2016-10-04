91fce69f0187f8b60d133eb3547af3406f3f43b1
Kean Mariotti <mariotti@cscs.ch>
2016-10-04 13:39:36 +0200

    activate_gpu_support.sh now parses command line arguments

--- src/activate_gpu_support.sh
+++ src/activate_gpu_support.sh
@@ -4,10 +4,11 @@
 #with an empty environment (after calling the clearenv function)
 export PATH=/usr/local/bin:/usr/bin:/bin:/sbin
 
-#TODO: replace these variables with parameters passed to the script by the shifter executable
-container_mount_point=/var/udiMount
-nvidia_libs_mount_point=$container_mount_point/gpu-support/lib/nvidia
-nvidia_bins_mount_point=$container_mount_point/gpu-support/bin/nvidia
+gpu_ids=
+container_mount_point=
+nvidia_bin_path=
+nvidia_lib_path=
+nvidia_lib64_path=
 
 #the NVIDIA compute libraries that will be bind mounted into the container
 nvidia_compute_libs="cuda \
@@ -24,12 +25,39 @@ nvidia_binaries="nvidia-cuda-mps-control \
                nvidia-persistenced \
                nvidia-smi"
 
-__log()
+log()
 {
     local level="$1"
     local msg="$2"
 
     printf "[ GPU SUPPORT ACTIVATION ] =$level= $msg\n" >&2
+}
+
+exit_if_previous_command_failed()
+{
+    if [ $? -ne 0 ]; then
+        log ERROR "internal error"
+        exit 1
+    fi
+}
+
+parse_command_line_arguments()
+{
+    if [ $# -ne 5 ]; then
+        log ERROR "internal error: received bad number of command line arguments"
+        exit 1
+    fi
+    
+    gpu_ids=$(echo $1 | sed 's/,/ /g')
+    container_mount_point=$2
+    nvidia_bin_path=$3
+    nvidia_lib_path=$4
+    nvidia_lib64_path=$5
+
+    if [ ! -d $container_mount_point ]; then
+        log ERROR "internal error: received invalid value for 'mount point' command line argument"
+        exit 1
+    fi
 }
 
 check_prerequisites()
@@ -38,53 +66,61 @@ check_prerequisites()
 
     for cmd in $cmds; do
         command -v $cmd >/dev/null && continue
-        __log ERROR "Command not found: $cmd"
+        log ERROR "Command not found: $cmd"
         exit 1
     done
 }
 
 add_nvidia_compute_libs_to_container()
 {
-    mkdir -p $nvidia_libs_mount_point/lib
-    mkdir -p $nvidia_libs_mount_point/lib64
+    local lib_path_container=$container_mount_point/$nvidia_lib_path
+    local lib64_path_container=$container_mount_point/$nvidia_lib64_path
+    
+    mkdir -p $lib_path_container
+    exit_if_previous_command_failed
+    mkdir -p $lib64_path_container
+    exit_if_previous_command_failed
 
     for lib in $nvidia_compute_libs; do
-        local lib_paths=$( ldconfig -p | grep "lib${lib}.so" | awk '{print $4}' )
-        if [ -z "$lib_paths" ]; then
-            __log WARN "could not find library: $lib"
+        local libs_host=$( ldconfig -p | grep "lib${lib}.so" | awk '{print $4}' )
+        if [ -z "$libs_host" ]; then
+            log WARN "could not find library: $lib"
             continue
         fi
         
-        for lib_path in $lib_paths; do
-            local lib_arch=$( file -L $lib_path | awk '{print $3}' | cut -d- -f1 )
-            if [ "$lib_arch" = "32" ]; then
-                local lib_mount_point=$nvidia_libs_mount_point/lib/$(basename $lib_path)
-            elif [ "$lib_arch" = "64" ]; then
-                local lib_mount_point=$nvidia_libs_mount_point/lib64/$(basename $lib_path)      
+        for lib_host in $libs_host; do
+            local arch=$( file -L $lib_host | awk '{print $3}' | cut -d- -f1 )
+            if [ "$arch" = "32" ]; then
+                local lib_container=$lib_path_container/$(basename $lib_host)
+            elif [ "$arch" = "64" ]; then
+                local lib_container=$lib64_path_container/$(basename $lib_host)
             else
-                __log ERROR "found/parsed invalid CPU architecture of NVIDIA library"
+                log ERROR "found/parsed invalid CPU architecture of NVIDIA library"
                 exit 1
             fi
             
-            touch $lib_mount_point
-            mount --bind $lib_path $lib_mount_point
+            touch $lib_container
+            mount --bind $lib_host $lib_container
         done
     done
 }
 
 add_nvidia_binaries_to_container()
 {
-    mkdir -p $nvidia_bins_mount_point
+    local bins_path_container=$container_mount_point/$nvidia_bin_path
+
+    mkdir -p $bins_path_container
+    exit_if_previous_command_failed
 
     for bin in $nvidia_binaries; do
-        local bin_path="$( which $bin )"
-        if [ -z $bin_path ]; then
-            __log WARN "could not find binary: $bin"
+        local bin_host="$( which $bin )"
+        if [ -z $bin_host ]; then
+            log WARN "could not find binary: $bin"
             continue
         fi
-        local bin_mount_point=$nvidia_bins_mount_point/$bin
-        touch $bin_mount_point
-        mount --bind $bin_path $bin_mount_point
+        local bin_container=$bins_path_container/$bin
+        touch $bin_container
+        mount --bind $bin_host $bin_container
     done
 }
 
@@ -95,6 +131,7 @@ load_nvidia_uvm_if_necessary()
     fi
 }
 
+parse_command_line_arguments $*
 check_prerequisites
 add_nvidia_compute_libs_to_container
 add_nvidia_binaries_to_container
