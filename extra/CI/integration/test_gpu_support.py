14cb52a6af6395fa9c954e6373301fe6caaed306
Kean Mariotti <mariotti@cscs.ch>
2016-12-07 15:59:40 +0100

    gpu support integration test: minor refactoring
    Renamed the dummy libraries used for the GPU support's integration
    tests, since they will be reused for the MPI support's integration
    tests.

--- extra/CI/integration/test_gpu_support.py
+++ extra/CI/integration/test_gpu_support.py
@@ -63,10 +63,10 @@ class TestGPUDevices(unittest.TestCase):
         for gpu_lib in cls._GPU_LIBS:
             if not os.path.exists("/lib/" + gpu_lib):
                 cls._created_gpu_libs.add("/lib/" + gpu_lib)
-                subprocess.call(["sudo", "cp", path + "/gpu_support_dummy_32bit_library.so", "/lib/" + gpu_lib])
+                subprocess.call(["sudo", "cp", path + "/libdummy32bit.so", "/lib/" + gpu_lib])
             if not os.path.exists("/lib64/" + gpu_lib):
                 cls._created_gpu_libs.add("/lib64/" + gpu_lib)
-                subprocess.call(["sudo", "cp", path + "/gpu_support_dummy_64bit_library.so", "/lib64/" + gpu_lib])
+                subprocess.call(["sudo", "cp", path + "/libdummy64bit.so", "/lib64/" + gpu_lib])
         subprocess.call(["sudo", "sed", "-i", "/etc/ld.so.conf", "-e", "$a/lib64"]) # append /lib64 to ld.so.conf
         subprocess.call(["sudo", "ldconfig"])
         subprocess.call(["sudo", "sed", "-i", "/etc/ld.so.conf", "-e", "$d"]) # remove /lib64 from ld.so.conf
