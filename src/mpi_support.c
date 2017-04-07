fc24f9e0b61c88e5e80b14d60c9b7165189cd35b
Kean Mariotti <mariotti@cscs.ch>
2017-04-07 15:00:25 +0200

    mpi support: handle empty MPI configuration options

--- src/mpi_support.c
+++ src/mpi_support.c
@@ -5,21 +5,24 @@
 #include "utility.h"
 #include "shifter_core.h"
 
-int execute_hook_to_activate_mpi_support(int verbose, const UdiRootConfig* udiConfig)
-{
+static char* make_non_empty_argument(char* arg) {
+    if(arg==NULL || strcmp(arg, "")==0) {
+        return strdup(";");
+    }
+    else {
+        return arg;
+    }
+}
+
+
+int execute_hook_to_activate_mpi_support(int verbose, const UdiRootConfig* udiConfig) {
     int ret = 0;
 
-    if(udiConfig->mpi_config.is_mpi_support_enabled)
-    {
-        if (udiConfig->siteMPISharedLibs == NULL || strlen(udiConfig->siteMPISharedLibs) == 0){
+    if(udiConfig->mpi_config.is_mpi_support_enabled) {
+        if (udiConfig->siteMPISharedLibs == NULL || strlen(udiConfig->siteMPISharedLibs) == 0) {
             fprintf(stderr, "Native MPI support requested but no site-specific MPI libraries defined\n");
             return 1;
         }
-
-        /* Ensure optional arguments (dependencies and configs) are not NULL */
-        char *dep_arg, *config_arg;
-        dep_arg = (udiConfig->siteMPIDependencyLibs == NULL) ? strdup(";") : strdup(udiConfig->siteMPIDependencyLibs);
-        config_arg = (udiConfig->siteMPIConfigurationFiles == NULL) ? strdup(";") : strdup(udiConfig->siteMPIConfigurationFiles);
 
         char* args[9];
         args[0] = strdup("/bin/bash");
@@ -27,16 +30,15 @@ int execute_hook_to_activate_mpi_support(int verbose, const UdiRootConfig* udiCo
         args[2] = strdup(udiConfig->udiMountPoint);
         args[3] = strdup(udiConfig->siteResources);
         args[4] = strdup(udiConfig->siteMPISharedLibs);
-        args[5] = dep_arg;
-        args[6] = config_arg;
+        args[5] = make_non_empty_argument(udiConfig->siteMPIDependencyLibs);
+        args[6] = make_non_empty_argument(udiConfig->siteMPIConfigurationFiles);
         args[7] = verbose ? strdup("verbose-on") : strdup("verbose-off");
         args[8] = NULL;
 
         /* Call the script to activate MPI support, then free argument pointers */
         ret = forkAndExecv(args);
         char** p;
-        for (p=args; *p != NULL; p++)
-        {
+        for (p=args; *p != NULL; p++) {
             free(*p);
         }
     }
@@ -44,8 +46,7 @@ int execute_hook_to_activate_mpi_support(int verbose, const UdiRootConfig* udiCo
     return ret;
 }
 
-int fprint_mpi_support_config(FILE* fp, const struct mpi_support_config* config)
-{
+int fprint_mpi_support_config(FILE* fp, const struct mpi_support_config* config) {
     size_t written = 0;
     written += fprintf(fp, "***** MPI support config *****\n");
     written += fprintf(fp, "is_mpi_support_enabled = %d\n", config->is_mpi_support_enabled);
