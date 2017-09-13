b2680ff1f95c3047af0b597d2dd7c1ae2cf2d7ec
lagerhardt <lgerhardt@lbl.gov>
2017-09-13 10:38:57 -0700

    Added fasthash to testing suite

---
+++ imagegw/test/fasthash.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import hashlib
+import argparse
+
+parser = argparse.ArgumentParser(description='Calculate file hash')
+parser.add_argument('infile', nargs=1, type=str)
+
+args = parser.parse_args()
+infile = args.infile[0]
+
+"""                                                                                                                      
+Calculate a sha256 hash of the first MB of a file and every 512MB offset
+"""    
+
+m = hashlib.sha256()
+with open(infile, 'rb', 1024 * 1024) as f:
+    l = f.read(1024 * 1024)
+    while (len(l) > 0):
+        m.update(l)
+        f.seek(1024 * 1024 * (512 - 1), 1)
+        l = f.read(1024 * 1024)
+
+print m.hexdigest()
