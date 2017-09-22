5987f14f000d0beaee11abb0a25a9fdb3b625722
lagerhardt <lgerhardt@lbl.gov>
2017-09-21 17:27:50 -0700

    removed bad colon

--- imagegw/test/imagegwapi_test.py
+++ imagegw/test/imagegwapi_test.py
@@ -260,7 +260,7 @@ class GWTestCase(unittest.TestCase):
         rv = self.app.post(uri, headers={AUTH_HEADER: self.auth},
                            data=datajson)
         data = json.loads(rv.data)
-        assert data['status'] == 'READY':
+        assert data['status'] == 'READY'
 #        assert 'filepath' in data
 #        assert rv.status_code == 200
 
