a8b8b43f0f8b58019e4121cb4b308ff3c831f6b2
Doug Jacobsen <dmjacobsen@lbl.gov>
2019-06-29 02:31:19 -0700

    Fix bug in way system dependencies were copied into musl installation
    In case of SLES symlinks it was also trying to copy over the files that were already copied

--- dep/build_ssh.sh
+++ dep/build_ssh.sh
@@ -63,8 +63,9 @@ for dir in $dirs; do
             # SLES has symlinks for asm
             realpath=$(readlink -f "/usr/include/$dir")
             cp -rp "$realpath" "${SPRT_PREFIX}/include/"
+	else
+            cp -rp "/usr/include/$dir" "${SPRT_PREFIX}/include/"
         fi
-        cp -rp "/usr/include/$dir" "${SPRT_PREFIX}/include/"
     fi
 done
 
