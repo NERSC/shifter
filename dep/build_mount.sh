6056abbc7045f33b18df65f4dd4219064294a5fe
Doug Jacobsen <dmjacobsen@lbl.gov>
2019-06-29 02:30:54 -0700

    Update to latest util-linux to get support for SLES15

--- dep/build_mount.sh
+++ dep/build_mount.sh
@@ -9,17 +9,17 @@ mkdir -p build_mount
 cd build_mount
 builddir=$( pwd )
 
-if [[ -n "$DEPTAR_DIR" && -e "$DEPTAR_DIR/util-linux-2.26.2.tar.gz" ]]; then
-    cp "$DEPTAR_DIR/util-linux-2.26.2.tar.gz" .
+if [[ -n "$DEPTAR_DIR" && -e "$DEPTAR_DIR/util-linux-2.34.tar.gz" ]]; then
+    cp "$DEPTAR_DIR/util-linux-2.34.tar.gz" .
 fi
 
-if [[ ! -e "util-linux-2.26.2.tar.gz" ]]; then
-    curl -L -k -o "util-linux-2.26.2.tar.gz" "https://www.kernel.org/pub/linux/utils/util-linux/v2.26/util-linux-2.26.2.tar.gz"
+if [[ ! -e "util-linux-2.34.tar.gz" ]]; then
+    curl -L -k -o "util-linux-2.34.tar.gz" "https://www.kernel.org/pub/linux/utils/util-linux/v2.34/util-linux-2.34.tar.gz"
 fi
 
 cd "${builddir}"
 mkdir -p util-linux
-tar xf "util-linux-2.26.2.tar.gz" -C util-linux --strip-components=1
+tar xf "util-linux-2.34.tar.gz" -C util-linux --strip-components=1
 cd util-linux
 CC=gcc ./configure --enable-static --disable-shared
 CC=gcc make mount
