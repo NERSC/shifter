ff923b6f6d737cbf91dfeffe1a7c58e98c35298a
Douglas Michael Jacobsen <dmjacobsen@lbl.gov>
2019-10-10 13:25:23 -0700

    fix spacing

--- src/VolumeMap.c
+++ src/VolumeMap.c
@@ -589,7 +589,7 @@ int _validateVolumeMap(
         const char *from,
         const char *to,
         VolumeMapFlag *flags,
-	const char **toExactAllowed,
+        const char **toExactAllowed,
         const char **toStartsWithDisallowed,
         const char **toExactDisallowed,
         const char **fromStartsWithDisallowed,
