ffd77e792d2d23b5fe772252a2ba5d00a9fceaaf
Shane Canon <scanon@lbl.gov>
2026-06-04 13:27:57 -0700

    Fix bit rot

--- dep/cpputest.sh
+++ dep/cpputest.sh
@@ -18,7 +18,7 @@ mkdir -p cpputest_src
 tar xf "cpputest-${CPPUTEST_VERSION}.tar.gz" -C cpputest_src --strip-components=1
 cd cpputest_src
 # make sure we have the last config.guess available, this helps when building on openpower
-curl 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' -o config.guess
+curl -L 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' -o config.guess
 ./configure --prefix="${baseDir}/cpputest"
 make
 make install
